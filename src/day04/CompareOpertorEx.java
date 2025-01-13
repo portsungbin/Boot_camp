f47755d4559a899530dd365f9beac1bfd8d5b90c
portsungbin <sd61224@gmail.com>
2025-01-13 12:39:20 +0900

    commit

--- src/day04/CompareOpertorEx.java
+++ src/day04/CompareOpertorEx.java
@@ -1,5 +1,7 @@
 package day04;
 
+import java.util.Scanner;
+
 public class CompareOpertorEx {
     public static void main(String[] args) {
 
@@ -24,6 +26,38 @@ public class CompareOpertorEx {
         //6. n1과 n2의 값이 n1이 n2보다 큰지를 비교하여 결과를 boolean result1에 저장 후 result1 출력
         result1 = n1 > n2;
         System.out.println(result1);
+
+//        //7. jongol 연산자 자가진단 5번
+//
+//        Scanner scan = new Scanner(System.in);
+//        int a = scan.nextInt();
+//        int b = scan.nextInt();
+//        System.out.println(a == b ? 1 : 0);
+//        System.out.println(a != b ? 1 : 0);
+
+        //문자 비교
+        char ch1 = 'A';
+        char ch2 = 'B';
+        result1 = (ch1 < ch2);
+        System.out.println(result1);
+
+        //실수 비교
+        int n5 = 1;
+        double n6 = 1.0;
+        result1 = (n5 == n6);
+        System.out.println(result1);
+
+        float n7 = 0.1f;
+        n6 = 0.1;
+        result1 = (n7 == n6);
+        System.out.println(result1);
+        result1 = ((float)n7 == n6);
+        System.out.println(result1);
+//부동소수점 값의 정확도 차이로 인한 비교 오류로 다른수로봄
+
+
+
+
 
 
     }
--- src/day04/Main.java
+++ src/day04/Main.java
@@ -6,16 +6,10 @@ public class Main {
     public static void main(String[] args) {
         //1. Scanner 선택하여 입력기 생성
         Scanner scan = new Scanner(System.in);
-        int result;
         int a = scan.nextInt();
         int b = scan.nextInt();
-
-        result = a + 100;//
-        System.out.print(result + " ");
-        result = b % 10;
-        System.out.println(result);
-        //5. 출력포멧에 맞도록 출력
-
+        System.out.println(a == b ? 1 : 0);
+        System.out.println(a != b ? 1 : 0);
 
     }
 }
