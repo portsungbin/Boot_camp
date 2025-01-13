6d8d36415cd5ab8633c38c603b21f00bead84d29
portsungbin <sd61224@gmail.com>
2025-01-13 16:53:23 +0900

    commit

--- src/day04/Main.java
+++ src/day04/Main.java
@@ -3,13 +3,18 @@ package day04;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        //1. Scanner 선택하여 입력기 생성
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        System.out.println(a == b ? 1 : 0);
-        System.out.println(a != b ? 1 : 0);
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a, b, c, d;
+
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+        d = scan.nextInt();
+
+        System.out.print(a > c && b > d );
     }
 }
+
