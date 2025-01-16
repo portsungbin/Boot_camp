8e32e73565ce4ac825b314b355a2920ecd36f0f0
portsungbin <sd61224@gmail.com>
2025-01-16 17:42:06 +0900

    commit

--- src/day06/Main.java
+++ src/day06/Main.java
@@ -6,26 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int[] a = new int[10];
-        int sum1 = 0;  // 짝수 번째 인덱스 합
-        int sum2 = 0;  // 홀수 번째 인덱스 합
-        int count = 0; // 홀수 번째 인덱스의 개수
-
+        char[] a = new char[10];
         for (int i = 0; i < a.length; i++) {
-            a[i] = scan.nextInt();
-
-            if (i % 2 == 0) {
-                sum1 += a[i];  // 짝수 번째 인덱스 값은 sum1에 합산
-            } else {
-                sum2 += a[i];  // 홀수 번째 인덱스 값은 sum2에 합산
-                count++;        // 홀수 번째 인덱스의 개수 증가
-            }
+            a[i] = scan.next().charAt(0);
         }
 
-        // 홀수 번째 값들의 평균 계산 (count가 0일 경우 방어 코드 추가)
-        double avg = (double) sum2 / count;
+        for (int i = a.length-1; i >= 0; i--) {
+            System.out.print(a[i] + " ");
+        }
 
-        System.out.println("sum : " + sum2);  // 홀수 번째 값들의 합 출력
-        System.out.printf("avg : %.1f", avg);  // 홀수 번째 값들의 평균 출력
+
     }
 }
